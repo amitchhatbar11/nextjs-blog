60b01835daf3e055b5933793b7749f76ce64f32a
Amit Chhatbar <amitchhatbar11@gmail.com>
2021-01-19 16:53:52 +0530

    name added

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Your Name'
+const name = 'Amit Chhatbar'
 export const siteTitle = 'Amit Chhatbar Nextjs Demo'
 
 export default function Layout({ children, home }) {
